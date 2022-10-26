c416b6b056e860faf94c3088a4f1eb1bca296587
ymzayek <yasmin.mzayek@inria.fr>
2022-10-26 21:11:44 +0200

    [circle full] Remove plotly dependency

--- build_tools/github/build_docs_dependencies.sh
+++ build_tools/github/build_docs_dependencies.sh
@@ -9,4 +9,4 @@ python -m pip install --user --upgrade --progress-bar off pip setuptools
 # Install the local version of the library, along with both standard and testing-related dependencies
 # The `doc` dependency group is included because the build_docs job uses this script.
 # See setup.cfg for dependency group options
-python -m pip install .[plotting,plotly,test,doc]
+python -m pip install .[plotting,test,doc]
