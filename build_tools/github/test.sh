47c3d9b29cbc9f78361c6bf9f5eee265259c8130
Yasmin <63292494+ymzayek@users.noreply.github.com>
2023-08-23 14:10:36 +0200

    Revert "Maint/depr warning as error (#21)"
    This reverts commit e0f76259e0fa1bacc40364d951168577c03ea2bd.

--- build_tools/github/test.sh
+++ build_tools/github/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-if [ ! -z "$PIP_FLAGS" ]; then
+if [[ $PIP_FLAGS ]]; then
     python -m pytest --pyargs nilearn --cov=nilearn -W error::DeprecationWarning,error::PendingDeprecationWarning
 else
     python -m pytest --pyargs nilearn --cov=nilearn
