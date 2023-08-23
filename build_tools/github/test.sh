584d50bc98a8922c36c75a06387d1730b69695b4
ymzayek <yasmin.mzayek@inria.fr>
2023-08-23 14:08:12 +0200

    Fix if command

--- build_tools/github/test.sh
+++ build_tools/github/test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 
-if [[ $PIP_FLAGS ]]; then
+
+if [ ! -z "$PIP_FLAGS" ]; then
     python -m pytest --pyargs nilearn --cov=nilearn -W error::DeprecationWarning,error::PendingDeprecationWarning
 else
     python -m pytest --pyargs nilearn --cov=nilearn
