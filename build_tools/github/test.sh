b9179daf3398b1f250810459c6b1bcbff7ef650b
ymzayek <yasmin.mzayek@inria.fr>
2023-08-29 17:07:30 +0200

    test

--- build_tools/github/test.sh
+++ build_tools/github/test.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 
-python -m pytest --pyargs nilearn --cov=nilearn
+#
+if [ ! -z "$PIP_FLAGS" ]; then
+    echo "Running $PIP_FLAGS test..."
+    python -m pytest --pyargs nilearn --cov=nilearn \
+        -W error::DeprecationWarning:matplotlib: \
+        -W error::PendingDeprecationWarning:matplotlib: \
+        -W error::DeprecationWarning:numpy: \
+        -W error::PendingDeprecationWarning:numpy:
+else
+    python -m pytest --pyargs nilearn --cov=nilearn
+fi
